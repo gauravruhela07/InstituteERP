e3f66b6ac2ebc2851788a09dcb63569739254c35
singh-amber <ambersinghraghuvanshi@gmail.com>
2020-09-17 09:51:45 +0530

    Admin UI First Commit

--- Course-Registration-Automation-System/src/components/Admin/admin.js
+++ Course-Registration-Automation-System/src/components/Admin/admin.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+import Navbar from "./adminComponents/navbar";
+import profile from "./adminComponents/profile";
+import course from "./adminComponents/course";
+import faculty from "./adminComponents/faculty";
+
+
 
 const Admin = () => {
     return (
-        <div>
-            This is admin page!
-        </div>
-    )
+    	<Router>
+	        <div className = "container">
+	        	<Navbar/>
+	        	<br/>
+		        <Route path='/' exact component={profile}/>
+		        <Route path='/course' exact component={course}/>
+		        <Route path='/faculty' exact component={faculty}/>
+	        </div>
+        </Router>
+    );
 }
 
 export default Admin;
