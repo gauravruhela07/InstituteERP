b57da170ea42763a0bc20cabde55212250ce4da8
singh-amber <ambersinghraghuvanshi@gmail.com>
2020-10-14 17:26:52 +0530

    Modified server.js

--- Course-Registration-Automation-System/backend/server.js
+++ Course-Registration-Automation-System/backend/server.js
@@ -9,14 +9,35 @@ var port = 5000;
 app.use(cors()); // cors acts as middleware
 app.use(express.json()) // for parsing the sent and received json
 
-const uri = 'mongodb://127.0.0.1:27017/';
 
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+/////////////////////////////////////////////Gaurav Ruhela will connect using this
+// const uri = 'mongodb://127.0.0.1:27017/';
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB Connection is established successfully');
+// mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('MongoDB Connection is established successfully');
+// });
+///////////////////////////////////////////
+
+
+
+
+
+//////////////////////////////////////////////Amber will connect using this
+mongoose.connect("mongodb://localhost:27017/course_registration", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error) => {
+    if (!error) {
+        console.log("Connected");
+    }
+    else {
+        console.log("Not connected");
+    }
 });
+//////////////////////////////////////////////
+
+
+
 
 // Adding routers for the DB models
 
@@ -26,6 +47,13 @@ const student_subjectRouter = require('./studentRoutes/student_subject');
 
 app.use('/student', studentRouter);
 app.use('/student_subject', student_subjectRouter);
+
+
+// Admin
+const coursesRouter = require('./adminRoutes/coursesCRUD');
+
+app.use('/course', coursesRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
